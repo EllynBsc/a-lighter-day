407befae98474daa3e2aa24efa312bbbd73b51d3
Inou Ridder <inouridder@gmail.com>
2017-12-14 17:15:19 +0100

    new styling about page

--- source/javascripts/about.js
+++ source/javascripts/about.js
@@ -1,12 +1,16 @@
-
-
-  $(document).ready(function() {
-    $(window).on('scroll', function() {
-        var st = $(this).scrollTop();
-
+$(document).ready(function() {
+  $(window).on('scroll', function() {
+      var st = $(this).scrollTop();
+      // if (st <= 800) {
         $('#box-one').css({
             'transform': 'translateY('+ (st/2) +'px)'
         });
-    });
+      // }
+      // if (st >= 800) {
+        // $('#box-three').css({
+        //   'transform': 'translateY('+ (st/2) +'px)'
+        // });
+      // }
+  });
 });
 
