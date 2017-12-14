6f170edfdb2f0e9488a9395214623ab37bf9feee
Inou Ridder <inouridder@gmail.com>
2017-12-14 23:04:21 +0100

    new design about page, removed excess margin on the right

--- source/javascripts/about.js
+++ source/javascripts/about.js
@@ -1,16 +1,9 @@
 $(document).ready(function() {
   $(window).on('scroll', function() {
-      var st = $(this).scrollTop();
-      // if (st <= 800) {
-        $('#box-one').css({
-            'transform': 'translateY('+ (st/2) +'px)'
-        });
-      // }
-      // if (st >= 800) {
-        // $('#box-three').css({
-        //   'transform': 'translateY('+ (st/2) +'px)'
-        // });
-      // }
+    var st = $(this).scrollTop();
+    $('#box-one').css({
+        'transform': 'translateY('+ (st/2) +'px)'
+    });
   });
 });
 
